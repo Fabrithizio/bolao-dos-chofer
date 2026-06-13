7f35d9333e1927c2497a5d5fdb9d663bec5584b5
Fabricio Silva <104510800+Fabrithizio@users.noreply.github.com>
2026-06-13 00:17:07 -0300

    Configura URL do Apps Script

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const API_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const API_URL = "https://script.google.com/macros/s/AKfycbzwq0Yql3SLoLHVKnC0fHto2mD6Q4amwmk0Ex9JknzM8tIQ5sC6EjvDVAE1TH6_xFmd/exec";
 const REFRESH_SECONDS = 15;
